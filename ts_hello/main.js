6d48e88a8df615f7023f1f4b49df68336dc65369
yeahsilver <gjdpdms2005@gmail.com>
2021-04-05 22:27:22 +0900

    Add contents

--- ts_hello/main.js
+++ ts_hello/main.js
@@ -1,10 +1,60 @@
+"use strict";
 // function doSomthing() {
 //     for (var i = 0; i < 5; i++) {
 //         console.log(i);
 //     }
+exports.__esModule = true;
 //     console.log('Finally: '+ i);
 // }
 // doSomthing();
-var count = 5;
-count = 'a';
-console.log(count);
+// typescript에서는 count가 number 형태라 count에 'a'를 삽입하면 에러.
+// 하지만 javascript에서는 에러가 나지 않음.
+// let count = 5;
+// count = 'a';
+// console.log(count);
+// enum같은 경우 javascript보다 typescript에서 더욱 간편하게 사용할 수 있음.
+// let a: number;
+// let b: boolean;
+// let c: string;
+// let d: any;
+// let e: number[] = [1,2,3];
+// let f: any[] = [1, true, 'a', false];
+// const ColorRed = 0;
+// const ColorGreen = 1;
+// const ColorBlue = 2;
+// enum Color { Red = 0, Green = 1, Blue = 2 };
+// let backgroundColor = Color.Red;
+// // type assertion
+// let message;
+// // message type = string
+// // <string> 또는 as string을 사용하여 message의 타입을 string이라고 알려줌
+// message = 'abc';
+// let endWithC = (<string>message).endsWith('c');
+// let alternativeWay = (message as string).endsWith('c');
+// let log = function(message) {
+//     console.log(message);
+// }
+// arrow function
+// let doLog = (message) => {
+//     console.log(message);
+// }
+// interface Point {
+//     x: number,
+//     y: number,
+//     draw: () => void
+// }
+// typescript class 사용법
+// class Point {
+//     // private x: number;
+//     // private y: number;
+//     constructor(private x?: number, private y?: number) {
+//         // this.x = x;
+//         // this.y = y;
+//     }
+//     draw() {
+//         console.log('X: ' + this.x + ', Y: ' + this.y);
+//     }
+// }
+var point_1 = require("./point");
+var point = new point_1.Point(1, 2);
+point.draw();
--- ts_hello/main.ts
+++ ts_hello/main.ts
@@ -14,3 +14,64 @@
 // count = 'a';
 
 // console.log(count);
+
+// enum같은 경우 javascript보다 typescript에서 더욱 간편하게 사용할 수 있음.
+// let a: number;
+// let b: boolean;
+// let c: string;
+// let d: any;
+// let e: number[] = [1,2,3];
+// let f: any[] = [1, true, 'a', false];
+
+// const ColorRed = 0;
+// const ColorGreen = 1;
+// const ColorBlue = 2;
+
+// enum Color { Red = 0, Green = 1, Blue = 2 };
+// let backgroundColor = Color.Red;
+
+// // type assertion
+// let message;
+
+// // message type = string
+// // <string> 또는 as string을 사용하여 message의 타입을 string이라고 알려줌
+// message = 'abc';
+// let endWithC = (<string>message).endsWith('c');
+// let alternativeWay = (message as string).endsWith('c');
+
+// let log = function(message) {
+//     console.log(message);
+// }
+
+// arrow function
+// let doLog = (message) => {
+//     console.log(message);
+// }
+
+// interface Point {
+//     x: number,
+//     y: number,
+//     draw: () => void
+// }
+
+
+// typescript class 사용법
+// class Point {
+//     // private x: number;
+//     // private y: number;
+    
+//     constructor(private x?: number, private y?: number) {
+//         // this.x = x;
+//         // this.y = y;
+//     }
+
+//     draw() {
+//         console.log('X: ' + this.x + ', Y: ' + this.y);
+//     }
+// }
+
+// 다른 파일 불러오기 및 가져오기
+// import { Point } from './point';
+
+// let point: Point = new Point(1, 2);
+// point.draw();
