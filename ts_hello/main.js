b97788e3a245f11233dce63f367ba4b1a2838a1c
yeahsilver <gjdpdms2005@gmail.com>
2021-04-05 21:44:26 +0900

    Add contents

---
+++ ts_hello/main.js
@@ -0,0 +1,10 @@
+// function doSomthing() {
+//     for (var i = 0; i < 5; i++) {
+//         console.log(i);
+//     }
+//     console.log('Finally: '+ i);
+// }
+// doSomthing();
+var count = 5;
+count = 'a';
+console.log(count);
---
+++ ts_hello/main.ts
@@ -0,0 +1,16 @@
+// function doSomthing() {
+//     for (var i = 0; i < 5; i++) {
+//         console.log(i);
+//     }
+
+//     console.log('Finally: '+ i);
+// }
+
+// doSomthing();
+
+// typescript에서는 count가 number 형태라 count에 'a'를 삽입하면 에러.
+// 하지만 javascript에서는 에러가 나지 않음.
+// let count = 5;
+// count = 'a';
+
+// console.log(count);
